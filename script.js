399e46d4eaddb3fce02e5b9e0500b711f790381c
nurticcip <nurticcip7@gmail.com>
2023-12-27 12:28:17 +0600

    .

---
+++ script.js
@@ -0,0 +1,30 @@
+const name = document.querySelector('.name')
+const age = document.querySelector('.age')
+const create = document.querySelector('.create')
+const list = document.querySelector('.list')
+const image = document.querySelector('.image')
+
+getData()
+
+create.addEventListener('click', () => addData())
+
+function addData() {
+    let obj = {
+        name: name.value,
+        age: age.value,
+        img: image.value
+    }
+
+    let data = JSON.parse(localStorage.getItem('person')) || []
+    data.push(obj)
+    localStorage.setItem('person', JSON.stringify(data))
+    getData()
+}
+
+function getData() {
+    let newData = JSON.parse(localStorage.getItem('person')) || []
+    newData.forEach((el) => {
+        list.innerHTML += `<h1>${el.name}</h1> <br> <h2>${el.age}</h2> <img src="${el.img}" alt="">`
+        
+    });
+}
